b1fc93ba8e6531243f54ab72ac760b666731896f
BerlincioniMatteo <fei.niccolo@itismeucci.com>
2017-11-21 06:32:55 +0100

    prova

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <time.h>
+#include <Windows.h>
+#include <process.h>
+
+using namespace std;
+
+void main ()
+{
+int v[10],c=0;
+for (int i=0;i<10;i++)
+{
+c++;
+cout<<c;
+}
+
+
+}
